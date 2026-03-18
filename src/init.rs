e168051d184d8b0ff6d042fc317423dfcd9200bf
Mohammed_maktap_Noor <mohammedalyfouad@gmail.com>
2026-03-18 15:43:01 +0200

    Connected to Aiven database end of e 3

--- src/init.rs
+++ src/init.rs
@@ -1,5 +1,13 @@
 use tracing_subscriber::{EnvFilter, FmtSubscriber};
 
+use std::str::FromStr; // For .from_str()
+use std::time::Duration; // For Duration::from_secs(5)
+
+use sqlx::{
+    postgres::{PgConnectOptions, PgPool, PgPoolOptions},
+    ConnectOptions,
+};
+
 pub fn logging() {
     let filter = EnvFilter::builder()
         .with_default_directive(tracing::Level::INFO.into())
@@ -12,6 +20,35 @@ pub fn logging() {
 
     tracing::subscriber::set_global_default(subscriber).expect("Failed to set up logging");
 }
+
+
+pub async fn database_connection() -> PgPool {
+    tracing::debug!("Setting up database connection");
+    let db_url = dotenvy::var("DATABASE_URL").expect("Failed to get database url from env");
+
+    let options = PgConnectOptions::from_str(&db_url)
+        .expect("failed to parse url")
+        .disable_statement_logging();
+
+    let pg_pool = PgPoolOptions::new()
+        .acquire_timeout(Duration::from_secs(5))
+        .connect_with(options)
+        .await
+        .expect("failed to connect to the database");
+
+    tracing::debug!("Successfully connected");
+
+    sqlx::migrate!()
+        .run(&pg_pool)
+        .await
+        .expect("Failed to migrate");
+
+    tracing::debug!("Successfully migrated");
+
+    pg_pool
+}
+
+
 //     let subscriber = FmtSubscriber::builder()
 //         .with_env_filter(filter)
 //         .finish();
